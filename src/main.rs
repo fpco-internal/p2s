fd0ec0d5af7390add88c949df75f483482f631da
Wang Shida <wangs@fpcomplete.com>
2023-06-20 19:20:18 +0800

    Using JSON data source

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use std::collections::HashSet;
-
 use anyhow::Result;
 use clap::Parser;
+use std::collections::HashSet;
 
 fn main() -> Result<()> {
     let opt = Opt::parse();
@@ -9,94 +8,113 @@ fn main() -> Result<()> {
         opt.client_key,
         sentry::ClientOptions {
             release: sentry::release_name!(),
+            session_mode: sentry::SessionMode::Request,
             ..Default::default()
         },
     ));
     sentry::configure_scope(|scope| scope.set_tag("bot-name", &opt.bot_name));
     let http_client = reqwest::blocking::Client::new();
-    let xpath_factory = sxd_xpath::Factory::new();
-    let h2 = xpath_factory.build("//h2/text()")?.unwrap();
-    let pre = xpath_factory.build("//pre/text()")?.unwrap();
-    let context = sxd_xpath::Context::new();
     let mut states: HashSet<(String, String)> = HashSet::new();
 
     loop {
         match http_client
             .get(&opt.url)
-            .header("accept", "text/html")
+            .header("accept", "application/json")
             .send()
         {
             Err(e) => {
-                eprintln!("{e:?}");
+                eprintln!("======= {e:?}");
                 sentry::capture_message(
                     &format!("Fetching status page error: {e:?}"),
                     sentry::Level::Error,
                 );
             }
             Ok(response) => {
-                let status_page = response.text()?;
-                let xdoc = sxd_html::parse_html(&status_page);
-                let xdoc = xdoc.as_document();
+                let statuses = response.json::<serde_json::Value>()?;
+                let statuses = statuses
+                    .as_object()
+                    .expect("Wrong JSON format 1")
+                    .get("statuses")
+                    .expect("Wrong JSON format 2")
+                    .as_array()
+                    .expect("Wrong JSON format 3");
 
-                let matches = sxd_xpath::evaluate_xpath(
-                    &xdoc,
-                    "/html/body/div/div/div/div/p[starts-with(text(), 'Status: ERROR')]/..",
-                )?;
-                if let sxd_xpath::Value::Nodeset(nodes) = matches {
-                    let errors: HashSet<(String, String)> = nodes
-                        .iter()
-                        .map(|i| {
-                            let title =
-                                if let sxd_xpath::Value::Nodeset(h2s) = h2.evaluate(&context, i)? {
-                                    h2s.iter()
-                                        .next()
-                                        .map(|x| x.string_value())
-                                        .unwrap_or_default()
-                                } else {
-                                    "".to_string()
-                                };
-                            let msg = if let sxd_xpath::Value::Nodeset(pres) =
-                                pre.evaluate(&context, i)?
-                            {
-                                pres.iter()
-                                    .next()
-                                    .map(|x| x.string_value())
-                                    .unwrap_or_default()
-                            } else {
-                                "".to_string()
-                            };
-                            eprintln!("{title}: {msg}");
-                            Ok((title, msg))
-                        })
-                        .collect::<Result<_>>()?;
+                let errors: HashSet<(String, String)> = statuses
+                    .iter()
+                    .filter_map(|i| {
+                        let i = i.as_object().expect("Wrong JSON format 4");
+                        let short = i
+                            .get("short")
+                            .expect("Wrong JSON format 13")
+                            .as_str()
+                            .expect("Wrong JSON format 14");
+                        if short == "error" || short == "out-of-date" {
+                            let label = i
+                                .get("label")
+                                .expect("Wrong JSON format 5")
+                                .as_str()
+                                .expect("Wrong JSON format 6");
+                            let last_result = i
+                                .get("status")
+                                .expect("Wrong JSON format 7")
+                                .as_object()
+                                .expect("Wrong JSON format 8")
+                                .get("last-result")
+                                .expect("Wrong JSON format 9")
+                                .as_object()
+                                .expect("Wrong JSON format 10")
+                                .get("value")
+                                .expect("Wrong JSON format 11")
+                                .as_object()
+                                .expect("Wrong JSON format 12");
+                            match last_result.get("Err") {
+                                // with `short` judged ahead, this should always be `Err`, right?
+                                None => {
+                                    // The Result is Ok
+                                    // Do nothing
+                                    None
+                                }
+                                Some(err) => {
+                                    let err = err.as_str().expect("Wrong JSON format 13");
+                                    Some((label.to_string(), err.to_string()))
+                                }
+                            }
+                        } else {
+                            None
+                        }
+                    })
+                    .collect();
 
-                    // New errors
-                    for (title, msg) in errors.difference(&states) {
-                        sentry::with_scope(
-                            |scope| scope.set_tag("part-name", title),
-                            || {
-                                sentry::capture_message(
-                                    &format!("{title}: {msg}"),
-                                    sentry::Level::Error,
-                                )
-                            },
-                        );
-                    }
-                    // Gone errors
-                    for (title, msg) in states.difference(&errors) {
-                        sentry::with_scope(
-                            |scope| scope.set_tag("part-name", title),
-                            || {
-                                sentry::capture_message(
-                                    &format!("Recoverred {title}: {msg}"),
-                                    sentry::Level::Info,
-                                )
-                            },
-                        );
-                    }
-
-                    states = errors;
+                // New errors
+                for (title, msg) in errors.difference(&states) {
+                    eprintln!("======= {title}: {msg}");
+                    sentry::with_scope(
+                        |scope| scope.set_tag("part-name", title),
+                        || {
+                            sentry::capture_message(
+                                &format!("{title}: {msg}"),
+                                sentry::Level::Error,
+                            )
+                        },
+                    );
                 }
+                // Gone errors
+                for (title, msg) in states.difference(&errors) {
+                    eprintln!("======= {title} Recoverred: {msg}");
+                    sentry::with_scope(
+                        |scope| scope.set_tag("part-name", title),
+                        || {
+                            sentry::capture_message(
+                                &format!("{title} Recoverred: {msg}"),
+                                sentry::Level::Info,
+                            )
+                        },
+                    );
+                }
+
+                eprintln!("####### {}", states.len());
+                states = errors;
+                eprintln!("####### {}", states.len());
             }
         }
 
